bce9499d90f8899a732dc965467ffa1f641deff9
Nikola Prokic <nproka@gmail.com>
2025-06-20 15:23:53 +0200

    test;

--- models/module_wizard.py
+++ models/module_wizard.py
@@ -1,25 +1,35 @@
-# simple_module/models/module_wizard.py
+# -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
+
 
 class SimpleModelListWizard(models.TransientModel):
     _name = 'simple.model.list.wizard'
-    _description = 'Wizard that shows names of selected simple.model records'
+    _description = 'Selected simple.model names'
 
-    record_names = fields.Text(string="Selected Simple-Model Names", readonly=True)
+    record_names = fields.Text(readonly=True)
+
+    simple_models = fields.Many2many('simple.model')
+
+    @api.model
+    def default_get(self, fields_list):
+        res = super().default_get(fields_list)
+        ids = self.env.context.get('active_ids', [])
+        names = self.env['simple.model'].browse(ids).mapped('name')
+        res['record_names'] = '\n'.join(names) if names else _('No records selected.')
+        return res
+
 
 class SimpleModel(models.Model):
     _inherit = 'simple.model'
 
     def action_show_selected_models(self):
-        names = "\n".join(self.mapped("name")) or _("No records selected.")
-        wiz = self.env['simple.model.list.wizard'].create({'record_names': names})
         view = self.env.ref('simple_module.view_simple_model_list_wizard_form')
         return {
             'name': _('Selected Simple-Model Names'),
             'type': 'ir.actions.act_window',
             'res_model': 'simple.model.list.wizard',
-            'res_id': wiz.id,
             'view_mode': 'form',
             'views': [(view.id, 'form')],
             'target': 'new',
+            'context': {'active_ids': self.ids},
         }
